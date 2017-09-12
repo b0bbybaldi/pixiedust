2d44cf49af1136e84257f63cef4830594e3483e7
Benjamin <benjamin.nutter@gmail.com>
2017-09-12 15:55:35 -0400

    what happens if I take out the htmlPreserve part?

--- R/print_dust_html.R
+++ R/print_dust_html.R
@@ -1,11 +1,3 @@
-#' @importFrom dplyr bind_rows
-#' @importFrom dplyr data_frame
-#' @importFrom dplyr select_
-#' @importFrom htmltools htmlPreserve
-#' @importFrom knitr asis_output
-#' @importFrom tidyr spread_
-#' 
-
 print_dust_html <- function(x, ..., asis=TRUE, 
                             linebreak_at_end = getOption("pixie_html_linebreak", 2),
                             interactive = getOption("pixie_interactive"))
@@ -93,8 +85,8 @@ print_dust_html <- function(x, ..., asis=TRUE,
   if (interactive & asis){
     getOption("viewer")(tmpfile)
   }
-  else if (asis) knitr::asis_output(htmltools::htmlPreserve(non_interactive))
-  else htmltools::htmlPreserve(non_interactive)
+  else if (asis) knitr::asis_output(non_interactive) #knitr::asis_output(htmltools::htmlPreserve(non_interactive))
+  else non_interactive #htmltools::htmlPreserve(non_interactive)
   
 }
 
